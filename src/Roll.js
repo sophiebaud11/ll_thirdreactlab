8b555d9d9048dbd53098cea10687fe698a759e0f
learninglab-fellows <fellows@learninglab.xyz>
2020-03-05 12:02:23 -0500

    clean code

--- src/Roll.js
+++ src/Roll.js
@@ -2,25 +2,11 @@ import React, { useState } from 'react'
 // import {  } from './styles.js'
 
 export default function Roll({ matchComments }) {
-  console.log(matchComments)
-  // const rollCommentsList = rollComments ? <div>
-  //   {rollComments.map((item,i) => <p style={{marginTop: "3px"}} key={i}>{rollComments[i].text + " (comment made at " + rollComments[i].time + ")"}</p>)}
-  // </div> : <p>No comments yet!</p>
-  // const matchCommentsList =  matchComments ? <div>
-  //   {matchComments.map((item, i) => <p style={{marginTop:"3px"}} key={i}>item.text</p> )}
-  // </div> : <p>No comments yet!</p>
-  // console.log(matchCommentsList)
-// see if this works
-
+ß
   const items = matchComments.map( (comment, i) => {
     return <p key={i}>{comment.text}</p>
   })
 
-  // const items = []
-  // for (const item of matchComments) {
-  //   items.push(item.text)
-  // }
-  console.log(items)
     return (
       <div>
         Comments:
